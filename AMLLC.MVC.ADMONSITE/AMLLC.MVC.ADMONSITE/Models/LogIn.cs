a64dd83b95689af27c457a14c5c4e84a7065321a
lauraPineda <laura.pineda1091@gmail.com>
2018-03-16 01:59:05 -0600

    Se agregan archivos de resuorces para cambio de lenguajes y se agrega vista, modelo y css para LogIn

---
+++ AMLLC.MVC.ADMONSITE/AMLLC.MVC.ADMONSITE/Models/LogIn.cs
@@ -0,0 +1,20 @@
+﻿using AMLLC.MVC.ADMONSITE.App_GlobalResources;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AMLLC.MVC.ADMONSITE.Models
+{
+    public class LogIn
+    {
+        [Required]
+        [Display(Name = "User", ResourceType = typeof(Strings))]
+        public string sUser { get; set; }
+        [Required]
+        [Display(Name = "Password", ResourceType = typeof(Strings))]
+        public string sPassword { get; set; }
+
+    }
+}
