fae91c0e2248c115bb4d639c55999ae8e66f97d7
lauraPineda <laura.pineda1091@gmail.com>
2018-03-18 00:02:42 -0600

    Se agrega archivos less

--- AMLLC.MVC.ADMONSITE/AMLLC.MVC.ADMONSITE/App_Start/BundleConfig.cs
+++ AMLLC.MVC.ADMONSITE/AMLLC.MVC.ADMONSITE/App_Start/BundleConfig.cs
@@ -2,10 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Optimization;
 
 namespace AMLLC.MVC.ADMONSITE.App_Start
 {
     public class BundleConfig
     {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            #region SkeletonLayout
+            bundles.Add(new StyleBundle("~/bundles/LogIn/Css").Include(
+                "~/Content/Library/Bootstrap/bootstrap.css",
+                "~/Content/OwnLibrary/Login/LogIn.css"
+                ));
+            #endregion
+        }
     }
 }
