a7c8e10767c1d6c9fb3f599d35688000d5af5e1e
lauraPineda <laura.pineda1091@gmail.com>
2018-03-26 14:35:28 -0600

    Se agregan controlladores

---
+++ AMLLC.MVC.ADMONSITE/AMLLC.MVC.ADMONSITE/Controllers/MenuController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AMLLC.MVC.ADMONSITE.Controllers
+{
+    public class MenuController : CustomLanguageController
+    {
+        // GET: Menu
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ AMLLC.MVC.ADMONSITE/AMLLC.MVC.ADMONSITE/Controllers/ReportController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AMLLC.MVC.ADMONSITE.Controllers
+{
+    public class ReportController : CustomLanguageController
+    {
+        // GET: Report
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
